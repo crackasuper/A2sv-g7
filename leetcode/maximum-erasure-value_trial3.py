1c87431e8bf9040d8aff88d716a58e4eddfb98cb
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-28 02:15:31 +0300

    Add solution for Maximum Erasure Value (trial 3)

---
+++ leetcode/maximum-erasure-value_trial3.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        dataset = set()
+        n = len(nums)
+        max_sum = 0
+        i = 0
+        j = 0
+        current_sum = 0
+        while j < n:
+            current_sum += nums[j]
+            while i <= j and nums[j] in dataset:
+                current_sum -= nums[i]
+                dataset.remove(nums[i])
+                i += 1
+            max_sum = max(max_sum, current_sum)
+            dataset.add(nums[j])
+            j += 1
+        return max_sum
